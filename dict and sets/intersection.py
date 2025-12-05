822ac7cd3055f1f92cd845cee94ae6677b0d05ca
Sam Prakash Royyala <sampraaksh1410@gmail.com>
2025-12-05 12:20:46 +0530

    added sets

---
+++ dict and sets/intersection.py
@@ -0,0 +1,7 @@
+set1={2,4,5,6}
+set2={4,6,7,8}
+set3={4,7,2}
+
+print("set1 intersection set2:",set1.intersection(set2))
+
+print("set1 intersection set2 intersection set 3:",set1.intersection(set2,set3))
